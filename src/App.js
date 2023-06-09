490f03cbb42b1a0198b803fbeb21a1791760d1c9
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-06-09 15:05:18 +0900

    Feat: 추가 헤더, 네비게이션 컴포넌트

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Users />} />
-      <Route path="/places/new" element={<NewPlace />} />
-    </Routes>
+    <>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
