e9790179e1629b5bfcde3f632ba00d7085b9743c
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-06-09 15:59:21 +0900

    Feat: 추가 사이드 드로어 in MainNavigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,32 @@
 import React from "react";
 import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
 import "./MainNavigation.css";
+import SideDrawer from "./SideDrawer";
 
 function MainNavigation(props) {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav>...</nav>
-    </MainHeader>
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 }
 
