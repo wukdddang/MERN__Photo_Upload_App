115c533d55e870af6c07f99e0a393d797b413cad
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-06-09 15:58:12 +0900

    Feat: 추가 사이드 드로어 컴포넌트

---
+++ src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import "./SideDrawer.css";
+
+function SideDrawer(props) {
+  const content = <aside className="side-drawer">{props.children}</aside>;
+  return createPortal(content, document.getElementById("drawer-hook"));
+}
+
+export default SideDrawer;
