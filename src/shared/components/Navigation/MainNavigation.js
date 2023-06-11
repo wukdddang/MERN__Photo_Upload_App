115a42c9d2953f8319957ad3c3ef8ea49238caa4
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-06-11 23:18:46 +0900

    Feat: 추가, 사이드 드로어 + Design: 추가, 반응형 사이드 드로어

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import MainHeader from "./MainHeader";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
 import "./MainNavigation.css";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 function MainNavigation(props) {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
   return (
     <>
-      <SideDrawer>
+      {/* 미리 정의해 둔 backdrop 컴포넌트를 띄워서 해당 지역 클릭 시 drawer가 사라지도록 */}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { createPortal } from "react-dom";
+import CSSTransition from "react-transition-group/CSSTransition";
 import "./SideDrawer.css";
 
 function SideDrawer(props) {
-  const content = <aside className="side-drawer">{props.children}</aside>;
+  const content = (
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+      onClick={props.onClick}>
+      <aside className="side-drawer">{props.children}</aside>
+    </CSSTransition>
+  );
   return createPortal(content, document.getElementById("drawer-hook"));
 }
 
